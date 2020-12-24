67d9e535af2a503157c65fc51633e0709ae16ab7
Matheus Santos Rosa Carneiro <matheus.carneiro@mundiale.com.br>
2020-12-23 22:13:03 -0300

    feat: created Produto Request and Response

--- crud/src/main/java/com/matheus/crud/modules/produto/response/ProdutoResponse.java
+++ crud/src/main/java/com/matheus/crud/modules/produto/response/ProdutoResponse.java
@@ -1,19 +1,34 @@
 package com.matheus.crud.modules.produto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.matheus.crud.modules.produto.request.ProdutoRequest;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = false)
-public class ProdutoResponse extends ProdutoRequest {
+@JsonPropertyOrder({"id", "nome", "estoque", "preco"})
+public class ProdutoResponse extends RepresentationModel<ProdutoResponse> implements Serializable {
+
+    private static final long serialVersionUID = -1546515674573168251L;
 
     @JsonProperty("id")
     private Long id;
+
+    @JsonProperty("nome")
+    private String nome;
+
+    @JsonProperty("estoque")
+    private Integer estoque;
+
+    @JsonProperty("preco")
+    private Double preco;
 
 }
