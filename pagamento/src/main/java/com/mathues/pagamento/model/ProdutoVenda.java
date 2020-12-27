042cbb9208a21c4f4015cc66c45c690d3c35da2d
Matheus Santos Rosa Carneiro <matheus.carneiro@mundiale.com.br>
2020-12-26 22:38:46 -0300

    feat: added produtoVenda entity and avoid possible stackoverflow

---
+++ pagamento/src/main/java/com/mathues/pagamento/model/ProdutoVenda.java
@@ -0,0 +1,38 @@
+package com.mathues.pagamento.model;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+
+@Entity
+@Table(catalog = "pagamento", name = "produto_venda")
+public class ProdutoVenda implements Serializable {
+
+    private static final long serialVersionUID = -3074636835534787453L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "id_produto", nullable = false, length = 10)
+    private Long idProduto;
+
+    @Column(name = "quantidade", nullable = false, length = 10)
+    private Integer quantidade;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_venda")
+    private Venda venda;
+
+}
