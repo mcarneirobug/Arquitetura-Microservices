26dcb42419f2d13f4ae9d45991dd9c1d29d00024
Matheus Santos Rosa Carneiro <matheus.carneiro@mundiale.com.br>
2020-12-26 22:37:38 -0300

    feat: added produto dto

---
+++ pagamento/src/main/java/com/mathues/pagamento/modules/produto/mapper/ProdutoMapper.java
@@ -0,0 +1,28 @@
+package com.mathues.pagamento.modules.produto.mapper;
+
+import com.mathues.pagamento.model.Produto;
+import com.mathues.pagamento.modules.produto.request.ProdutoRequest;
+import com.mathues.pagamento.modules.produto.response.ProdutoResponse;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ProdutoMapper {
+
+    public static Produto to(ProdutoRequest produtoRequest) {
+
+        final var produto = new Produto();
+        produto.setEstoque(produtoRequest.getEstoque());
+
+        return produto;
+    }
+
+    public static ProdutoResponse toResponse(Produto produto) {
+
+        final var produtoResponse = new ProdutoResponse();
+        produtoResponse.setId(produto.getId());
+        produtoResponse.setEstoque(produto.getEstoque());
+
+        return produtoResponse;
+    }
+}
