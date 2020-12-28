0eca555969a6faef79066cc48175d6a6dabd9380
Matheus Santos Rosa Carneiro <matheus.carneiro@mundiale.com.br>
2020-12-27 23:01:12 -0300

    feat: added rabbitmq config and producer

--- crud/src/main/java/com/matheus/crud/config/rabbitmq/MessageConfig.java
+++ crud/src/main/java/com/matheus/crud/config/rabbitmq/MessageConfig.java
@@ -1,2 +1,51 @@
-package com.matheus.crud.config.rabbitmq;public class MessageConfig {
+package com.matheus.crud.config.rabbitmq;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MessageConfig {
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
+    }
+
+    @Value("${crud.rabbitmq.exchange}")
+    private String exchange;
+
+    @Value("${crud.rabbitmq.routingkey}")
+    private String queue;
+
+    @Bean
+    public DirectExchange declareExchange() {
+        return new DirectExchange(exchange);
+    }
+
+    @Bean
+    public Queue declareQueue() {
+        return new Queue(queue, true);
+    }
+
+    @Bean
+    public Binding queue() {
+        return BindingBuilder
+                .bind(declareQueue())
+                .to(declareExchange())
+                .with(queue);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
 }
--- crud/src/main/java/com/matheus/crud/message/ProdutoSendMessage.java
+++ crud/src/main/java/com/matheus/crud/message/ProdutoSendMessage.java
@@ -1,2 +1,26 @@
-package com.matheus.crud.message;public class ProdutoSendMessage {
+package com.matheus.crud.message;
+
+import com.matheus.crud.modules.produto.response.ProdutoResponse;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProdutoSendMessage {
+
+    @Value("${crud.rabbitmq.exchange}")
+    private String exchange;
+
+    @Value("${crud.rabbitmq.routingkey}")
+    private String routingKey;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public ProdutoSendMessage(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(ProdutoResponse produtoResponse) {
+        rabbitTemplate.convertAndSend(exchange, routingKey, produtoResponse);
+    }
 }
