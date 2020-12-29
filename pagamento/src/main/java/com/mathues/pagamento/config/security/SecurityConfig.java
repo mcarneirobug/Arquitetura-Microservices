98bce6cee07675593a323fb4a0628c8d676c3cd7
Matheus Santos Rosa Carneiro <matheus.carneiro@mundiale.com.br>
2020-12-29 20:20:22 -0300

    feat: added jwt to project and configuration

---
+++ pagamento/src/main/java/com/mathues/pagamento/config/security/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.mathues.pagamento.config.security;
+
+
+import com.mathues.pagamento.jwt.JwtConfig;
+import com.mathues.pagamento.jwt.JwtTokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .authorizeRequests()
+                    .anyRequest().authenticated()
+                .and()
+                .apply(new JwtConfig(jwtTokenProvider));
+    }
+}
