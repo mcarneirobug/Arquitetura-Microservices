5d50f4ab92f8f8ff4aa92139eaa8e408aa476d82
Matheus Santos Rosa Carneiro <matheus.carneiro@mundiale.com.br>
2020-12-28 22:44:14 -0300

    feat: added dependency to jwt and JwtTokenProvider initial

---
+++ auth/src/main/java/com/matheus/auth/jwt/JwtTokenProvider.java
@@ -0,0 +1,93 @@
+package com.matheus.auth.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class JwtTokenProvider {
+
+    @Value("${security.jwt.token.secret-key}")
+    private String secretKey;
+
+    @Value("${security.jwt.token.expire-length}")
+    private Long expire;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    /**
+     * Método para criptograr a secretKey
+     */
+
+    @PostConstruct
+    protected void init() {
+        this.secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    public String createToken(String userName, List<String> roles) {
+        Claims claims = Jwts.claims().setSubject(userName);
+        claims.put("roles", roles);
+
+        Date now = new Date();
+        Date validate = new Date(now.getTime() + expire);
+
+        return Jwts
+                .builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validate)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    /**
+     * Método de obter a autenticação com o Token
+     */
+
+    public Authentication getAuthentication(String token) {
+
+        UserDetails userDetails = this.userDetailsService.loadUserByUsername(getUserName(token));
+
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    /**
+     * Método responsável por retornar o userName a partir do Token
+     * @param token
+     * @return userName
+     */
+    private String getUserName(String token) {
+        return Jwts
+                .parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject(); // conseguimos pegar o userName pelo seu subject
+    }
+
+    /**
+     * Método para resolver o Bearer
+     */
+
+    public String resolveToken(HttpServletRequest req) {
+        String bearerToken = req.getHeader("Authorization");
+        if(bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7, bearerToken.length());
+        }
+        return null;
+    }
+}
